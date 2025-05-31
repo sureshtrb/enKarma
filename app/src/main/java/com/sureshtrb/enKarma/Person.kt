a824519ec2f0cdacc50d044861a009c22ecfaa59
sureshtrb <sureshtrb@gmail.com>
2025-05-31 12:42:41 +0300

    Removed all traces of com.karma.sureshtrb.enKarma

---
+++ app/src/main/java/com/sureshtrb/enKarma/Person.kt
@@ -0,0 +1,1 @@
+package com.sureshtrb.enKarma  data class Person(     var name: String = "",     var birthOrder: String = "",     var gothram: String = "",     var gender: String = "",     var relationship: String = "" ) 
