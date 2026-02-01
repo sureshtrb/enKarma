5820f81d48fa403f7f803c887a01f695bcf832f9
sureshtrb <sureshtrb@gmail.com>
2026-02-01 18:31:29 +0300

    Create GlobalData.kt for shared application state
    Added global variables for astrological and family member status in the enKarma application.

---
+++ app/src/main/java/com/karma/sureshtrb/enKarma/GlobalData.kt
@@ -0,0 +1,22 @@
+package com.karma.sureshtrb.enKarma
+
+// Global variables for the enKarma application
+// Centralized location for all shared state across activities
+
+// Astrological/Panchang variables
+var shakaSamvat: String = ""
+var vedicAyana: String = ""
+var vedicRithu: String = ""
+var suryaRasi: String = ""
+var paksha: String = ""
+var weekDay: String = ""
+var naksha: String = ""
+var yoga: String = ""
+var karana: String = ""
+
+// Family member status variables
+var motherLive: String = ""
+var yrGFatherLive: String = ""
+var fatherMotherLive: String = ""
+var mFatherLive: String = ""
+var motherMotherLive: String = ""
