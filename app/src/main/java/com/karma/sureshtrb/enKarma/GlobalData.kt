eb3d51e8b6055ec6dd33cb1ad71a4c35966b12ae
sureshtrb <sureshtrb@gmail.com>
2026-02-01 21:18:08 +0300

    Add missing variable declarations to GlobalData

--- app/src/main/java/com/karma/sureshtrb/enKarma/GlobalData.kt
+++ app/src/main/java/com/karma/sureshtrb/enKarma/GlobalData.kt
@@ -26,3 +26,19 @@ var fatherMotherLive: String = ""
 var mFatherLive: String = ""
 var motherMotherLive: String = ""
 
+
+// Additional Panchang calculation variables
+var nextDayTithiHrToMin: Int = 0
+var nextDayTitiUptoHrOnly: Int = 0
+var todayPaksha: String = ""
+var mapKey: String = ""
+var DataList: String = ""
+var madyana: String = ""
+var prathaSanthya: String = ""
+var sayamSandya: String = ""
+var mapKey2: String = ""
+var DataList2: String = ""
+var mapKey3: String = ""
+var DataList3: String = ""
+var mapKey4: String = ""
+var DataList4: String = ""
