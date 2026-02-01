4089658a580bf4525bceaf24abee139deb0db64b
sureshtrb <sureshtrb@gmail.com>
2026-02-01 16:45:06 +0300

    Update Firestore settings for offline persistence

--- app/src/main/java/com/karma/sureshtrb/enKarma/App.kt
+++ app/src/main/java/com/karma/sureshtrb/enKarma/App.kt
@@ -6,7 +6,7 @@ import android.util.Log
 import com.google.firebase.FirebaseApp
 import com.google.firebase.database.ktx.database
 import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.settings
+import com.google.firebase.firestore.FirebaseFirestoreSettings
 import com.google.firebase.installations.FirebaseInstallations
 import com.google.firebase.ktx.Firebase
 
@@ -29,7 +29,7 @@ class App : Application() {
             Firebase.database.setPersistenceEnabled(true)
 
             // Optionally enable Firestore offline support
-            Firebase.firestore.firestoreSettings = settings { isPersistenceEnabled = true }
+            Firebase.firestore.firestoreSettings = FirebaseFirestoreSettings.Builder().setPersistenceEnabled(true).build()
 
             // Retrieve Firebase Installation ID
             FirebaseInstallations.getInstance().id
